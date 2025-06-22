ca60b67696ff470a5d0e486ac66cf72002262cbb
Moh. Yusril Maqoshidana <yusrilkingsr07@gmail.com>
2025-06-22 20:22:37 +0700

    [feat] add Publisher dan Consumer Service, perubahan beberapa configurasi kafka

--- TransactionService/src/main/java/com/microservice/transactionservice/models/TransactionModel.java
+++ TransactionService/src/main/java/com/microservice/transactionservice/models/TransactionModel.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Entity
 @NoArgsConstructor
@@ -13,12 +15,35 @@ public class TransactionModel {
     @Column(name = "transaction_id")
     private String transactionId;
 
-    @Column(name = "transactionName", nullable = false)
-    private String transactionName;
+    // Pengirim
+    @Column(name= "sender_user_id")
+    private String senderUserId;
 
-    @Column(name = "nominal", nullable = false)
-    private float nominal;
+    // Penerima
+    @Column(name= "receiver_user_id")
+    private String receiverUserId;
 
-    @Column(name = "status", nullable = false)
-    private boolean status;
+    // Jumlah Transfer
+    @Column(name= "amount")
+    private float amount;
+
+    // Selalu USER_TRANSFER
+    @Column(name= "transaction_type")
+    private String transactionType;
+
+    // Ketika di inisiasi
+    @Column(name = "created_at")
+    private String createdAt;
+
+    // Ketika proses selesai (bisa gagal atau sukses)
+    @Column(name = "completed_at")
+    private String completedAt;
+
+    // Deskripsi
+    @Column(name = "description")
+    private String description;
+
+    // Status ('PENDING', 'COMPLETED', 'FAILED', 'REFUNDED')
+    @Column(name = "status")
+    private String status;
 }
