caca12659d656f9c6807c5547e0aa20629340aed
lyeiva86 <ulul10039@gmail.com>
2025-06-23 23:08:17 +0700

    [FIX] clear service

--- TransactionService/src/main/java/com/microservice/transactionservice/repository/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservice.transactionservice.repository;
-
-import com.microservice.transactionservice.models.TransactionModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<TransactionModel, String> {
-    TransactionModel getTransactionModelByTransactionId(String transactionId);
-}
