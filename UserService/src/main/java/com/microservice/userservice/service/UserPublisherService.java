e1cbf8ee718aed8ba858b72d6b072f0911c7b4c0
Moh. Yusril Maqoshidana <yusrilkingsr07@gmail.com>
2025-06-24 12:33:50 +0700

    [fix] kafka create user

--- UserService/src/main/java/com/microservice/userservice/service/UserPublisherService.java
+++ UserService/src/main/java/com/microservice/userservice/service/UserPublisherService.java
@@ -3,6 +3,7 @@ package com.microservice.userservice.service;
 import com.microservice.userservice.DTO.UserCreatedEvent;
 import com.microservice.userservice.models.UserModel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -11,17 +12,15 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 
-import static org.apache.kafka.common.requests.DeleteAclsResponse.log;
-
-
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserPublisherService {
 
     @Value("${topic.kafka.user-created}")
     private String topicName;
 
-    private KafkaTemplate<String, UserCreatedEvent> kafkaTemplate;
+    private final KafkaTemplate<String, UserCreatedEvent> kafkaTemplate;
 
     public void publishUserCreatedEvent(UserModel userModel) {
         UserCreatedEvent event = UserCreatedEvent.builder()
