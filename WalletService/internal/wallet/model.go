5cb956edef4f68cb329b1b2e14d93d7ded60791d
lyeiva86 <ulul10039@gmail.com>
2025-06-24 15:15:10 +0700

    [FIX] model user_id

--- WalletService/internal/wallet/model.go
+++ WalletService/internal/wallet/model.go
@@ -8,7 +8,7 @@ import (
 
 type Wallet struct {
 	ID        uuid.UUID      `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
-	UserID    uuid.UUID      `gorm:"column:userId;type:uuid;not null;uniqueIndex" json:"userId"`
+	UserID    uuid.UUID      `gorm:"column:user_id;type:uuid;not null;uniqueIndex" json:"userId"`
 	Balance   float64        `gorm:"type:numeric(15,2);not null;default:0.00" json:"balance"`
 	Status    string         `gorm:"type:varchar(20);not null" json:"status"`
 	CreatedAt time.Time      `json:"created_at"`
